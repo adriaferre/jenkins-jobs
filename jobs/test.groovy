42096910b6749b919e520fd47e8ff4d5c334d965
Adrià Ferré <aferre@newrelic.com>
2019-08-11 14:17:05 +0200

    test with dummy job

---
+++ jobs/test.groovy
@@ -0,0 +1,11 @@
+job('DSL-TEST') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
